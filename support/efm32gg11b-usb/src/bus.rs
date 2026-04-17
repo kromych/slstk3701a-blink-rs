57132cd73b57451024c48143a10fd7eb1166fa98
kromych <kromych@users.noreply.github.com>
2026-04-16 19:00:23 -0700

    fix flush fifo

--- support/efm32gg11b-usb/src/bus.rs
+++ support/efm32gg11b-usb/src/bus.rs
@@ -436,12 +436,19 @@ impl UsbBus {
     }
 
     /// Flush EP0 IN TX FIFO if a transfer is pending.
+    ///
+    /// The DWC2 spec requires Global IN NAK to be set before flushing a
+    /// TX FIFO so the core does not transmit a partial packet while the
+    /// FIFO is being drained.
     pub fn flush_ep0_tx_if_pending(&self) {
         if self.usb.diep0tsiz().read().pktcnt().bits() != 0 {
+            self.usb.dctl().modify(|_, w| w.sgnpinnak().set_bit());
+            while !self.usb.dctl().read().gnpinnaksts().bit_is_set() {}
             self.usb
                 .grstctl()
                 .write(|w| w.txfflsh().set_bit().txfnum().f0());
             while self.usb.grstctl().read().txfflsh().bit_is_set() {}
+            self.usb.dctl().modify(|_, w| w.cgnpinnak().set_bit());
         }
     }
 
